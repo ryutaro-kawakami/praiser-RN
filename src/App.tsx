71f17916c98ac56d7953948b48da9f341442836e
ryutaro-kawakami <r-kawakami@mediba.jp>
2021-06-01 19:57:36 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//src/App.tsx
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Helloworld</Text>
+    </View>
+  );
+}
