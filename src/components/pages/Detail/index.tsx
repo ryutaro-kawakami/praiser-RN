33d3be7097f29c41fd8bbb5fd320c0c2b1c1f144
kwkm2000 <kwkm.0802@gmail.com>
2021-07-12 20:46:52 +0900

    ページ実装

--- src/components/pages/Detail/index.tsx
+++ src/components/pages/Detail/index.tsx
@@ -1,18 +1,104 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {useRoute, RouteProp, useNavigation} from '@react-navigation/native';
+// import analytics from '@react-native-firebase/analytics';
+import TextField, {dismiss} from '../../atoms/TextField';
+import Button from '../../atoms/Button';
+import {useControlledComponent} from '../../../lib/hooks';
+import {UiContext} from '../../../contexts';
+// import testIDs from '../../../constants/testIDs';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 10,
+  },
+  textField: {
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 20,
   },
 });
 
-export default function Home() {
+interface TodoEditActions {
+  changeTodo: (
+    id: string,
+    newValue: {
+      title: string;
+      detail: string;
+    },
+  ) => void;
+}
+
+interface Props {
+  actions: TodoEditActions;
+}
+
+interface Params {
+  id: string;
+  isEditable: boolean;
+  title: string;
+  detail: string;
+}
+
+export default function Detail() {
+  const {goBack} = useNavigation();
+  const {params} = useRoute<RouteProp<Record<string, Params>, string>>();
+  const {
+    isEditable,
+    title: titleInitialValue,
+    detail: detailInitialValue,
+  } = params;
+
+  const title = useControlledComponent(titleInitialValue);
+  const detail = useControlledComponent(detailInitialValue);
+
+  // const {setSnackbar} = React.useContext(UiContext);
+  const onSubmit = React.useCallback(() => {
+    goBack();
+    // props.actions.changeTodo(id, {
+    //   title: title.value,
+    //   detail: detail.value,
+    // });
+    // dismiss();
+    // setSnackbar({
+    //   visible: true,
+    //   message: 'edit is completed.',
+    //   label: 'Done',
+    // });
+  }, [goBack]);
+
+  // React.useEffect(() => {
+  //   async function logViewItem() {
+  //     await analytics().logViewItem({
+  //       item_id: id,
+  //       item_name: titleInitialValue,
+  //       item_category: 'todo',
+  //     });
+  //   }
+  //   logViewItem();
+  // }, [id, titleInitialValue]);
+
   return (
     <View style={styles.container}>
-      <Text>Detail</Text>
+      <TextField
+        disabled={!isEditable}
+        label="title"
+        value={title.value}
+        onChangeText={title.onChangeText}
+        style={styles.textField}
+      />
+      <TextField
+        disabled={!isEditable}
+        label="detail"
+        value={detail.value}
+        onChangeText={detail.onChangeText}
+        style={styles.textField}
+      />
+      {isEditable && (
+        <Button onPress={onSubmit} label="Submit" style={styles.button} />
+      )}
     </View>
   );
 }
--- src/components/pages/Input/index.tsx
+++ src/components/pages/Input/index.tsx
@@ -1,14 +1,97 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+} from 'react-native';
+import IconButton from '../../atoms/IconButton';
+import {useNavigation} from '@react-navigation/native';
+import TextField, {dismiss} from '../../atoms/TextField';
+import Button from '../../atoms/Button';
+import {COLOR} from '../../../constants/theme';
+// import testIDs from '../../../constants/testIDs';
+// import {Todo} from '../../../domain/models';
+import {useControlledComponent} from '../../../lib/hooks/';
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  text: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 20,
+  },
+  iconButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
 });
 
+// interface Props {
+//   actions: {
+//     addTodo: (newValues: Todo.Values) => void;
+//   };
+// }
+
 export default function Input() {
+  const title = useControlledComponent('');
+  const detail = useControlledComponent('');
+
+  const {goBack} = useNavigation();
+  const back = React.useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  const addTodo = React.useCallback(() => {
+    // props.actions.addTodo({
+    //   title: title.value,
+    //   detail: detail.value,
+    // });
+    back();
+    title.onChangeText('');
+    detail.onChangeText('');
+  }, [back, title, detail]);
+
   return (
-    <View style={styles.container}>
-      <Text>Input</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={dismiss}>
+        <View style={styles.container}>
+          <IconButton
+            icon="close"
+            size={30}
+            iconColor={COLOR.PRIMARY}
+            onPress={back}
+            style={styles.iconButton}
+          />
+          <TextField
+            label="Title"
+            value={title.value}
+            onChangeText={title.onChangeText}
+            style={styles.text}
+          />
+          <TextField
+            label="Detail"
+            value={detail.value}
+            onChangeText={detail.onChangeText}
+            style={styles.text}
+          />
+          <Button
+            onPress={addTodo}
+            label="Add"
+            style={styles.button}
+            disabled={!title.value}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
