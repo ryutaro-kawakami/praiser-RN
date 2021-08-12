b4ebad456dd00ac308487ddd545a74166cd3a954
kwkm2000 <kwkm.0802@gmail.com>
2021-08-12 19:47:41 +0900

    ui context実装

---
+++ src/components/molecules/NetworkPanel.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {ActivityIndicator, StyleSheet, SafeAreaView} from 'react-native';
+import {NetworkContext} from '../../contexts';
+import {height, width} from '../../lib/window';
+import {COLOR} from '../../constants/theme';
+
+const styles = StyleSheet.create({
+  dropdown: {
+    position: 'absolute',
+    width,
+    height,
+    backgroundColor: COLOR.WHITE,
+    opacity: 0.5,
+  },
+  container: {
+    position: 'absolute',
+    width,
+    height,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default function NetworkPanel() {
+  const {networkState} = React.useContext(NetworkContext);
+  const isCommunicating = React.useMemo(() => 0 < networkState, [networkState]);
+
+  if (!isCommunicating) {
+    return null;
+  }
+  return (
+    <>
+      <SafeAreaView style={styles.dropdown} />
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator size="large" color={COLOR.MAIN_LIGHT} />
+      </SafeAreaView>
+    </>
+  );
+}
