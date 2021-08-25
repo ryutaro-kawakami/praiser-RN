2d5531fa4e8c059a43d6df8a37e16fb459b22efa
kwkm2000 <kwkm.0802@gmail.com>
2021-08-25 15:29:15 +0900

    Loading page

---
+++ src/lib/firebase/firestore.ts
@@ -0,0 +1,5 @@
+import firestore from '@react-native-firebase/firestore';
+
+export default function getFirestore(uid: string) {
+  return firestore().collection('users').doc(uid).collection('todos');
+}
