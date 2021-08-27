b7810aaf1e732e39009e45966c062c6fc544b0b7
kwkm2000 <kwkm.0802@gmail.com>
2021-08-27 16:35:32 +0900

    InputページのContainer化とFirebaseへの書き込み

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,5 @@
 export {default as Home} from './Home';
 export {default as Detail} from './Detail';
+export {default as Input} from './Input';
 export {default as Loading} from './Loading';
 export {default as SignIn} from './SignIn';
