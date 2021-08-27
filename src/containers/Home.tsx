a922b8b5b14f52fb6cba0afa6b856ee62d1adf56
kwkm2000 <kwkm.0802@gmail.com>
2021-08-27 16:26:01 +0900

    Home周辺をContainerから読み込み

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+
 import {getTodos} from '../selectors/todos';
-import * as Todos from '../modules/todos';
+import * as Todos from '../usecases/todos';
 import {Home} from '../components/pages';
+import {UserContext} from '../contexts';
 
 export default function ConnectedHome() {
   const todos = useSelector(getTodos);
+  const {userState} = React.useContext(UserContext);
+
   const dispatch = useDispatch();
   const actions = React.useMemo(
-    () => ({
-      removeTodo(id: string) {
-        dispatch(Todos.remove(id));
-      },
-      toggleTodo(id: string) {
-        dispatch(Todos.toggle(id));
-      },
-    }),
-    [dispatch],
+    () =>
+      userState
+        ? {
+            removeTodo(id: string) {
+              dispatch(Todos.removeAndSync(userState.id, id));
+            },
+            toggleTodo(id: string) {
+              dispatch(Todos.toggleAndSync(userState.id, id));
+            },
+          }
+        : null,
+    [userState, dispatch],
   );
+
+  if (!actions) {
+    return null;
+  }
+
   return <Home todos={todos} actions={actions} />;
 }
